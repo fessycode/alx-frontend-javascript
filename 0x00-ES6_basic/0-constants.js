883564f10b26e9871601e91bd621a35129ef6358
fessycode <fessydoris@gmail.com>
2024-01-04 22:14:13 +0000

    first file

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,12 @@
+function taskFirst() {
+  // Using const to instantiate constants
+  const task = 'I prefer const when I can.';
+	return task
+}
+
+
+function taskNext() {
+  // Using const to instantiate constants
+  let nextTask = 'But sometimes let is okey';
+	return nextTask
+}

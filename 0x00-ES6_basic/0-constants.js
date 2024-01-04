9bd49432ae9507c8520154d3a75218b232a252d2
fessycode <fessydoris@gmail.com>
2024-01-04 22:34:39 +0000

    update

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,11 +1,11 @@
-function taskFirst() {
+export function taskFirst() {
   // Using const to instantiate constants
   const task = 'I prefer const when I can.';
 	return task
 }
 
 
-function taskNext() {
+export function taskNext() {
   // Using const to instantiate constants
   let nextTask = 'But sometimes let is okey';
 	return nextTask
